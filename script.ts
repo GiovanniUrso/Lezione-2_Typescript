8fd5206a84b7ef7304184141b1721d3ffe69a0b3
Giox367 <111007060+Giox367@users.noreply.github.com>
2023-01-05 23:40:33 +0100

    .

--- script.ts
+++ script.ts
@@ -1,1 +1,22 @@
-// Keyword modificatori di accesso, classi astratte e interfacce
+// Keyword modificatori di accesso, classi astratte e interfacce 
+
+class Persona{
+    nome:string;
+    congome:string;
+    eta:number;
+    coniugato:boolean
+    constructor(nome:string,cognome:string,eta:number,coniugato:boolean){
+        this.nome = nome;
+        this.congome = cognome;
+        this.eta = eta;
+        this.coniugato = coniugato
+    }
+
+    cresce():number{
+        return this.eta++;
+    }
+    stampaNomeSuSchermo():void{
+        const body:HTMLElement = <HTMLElement>document.getElementById('container')
+        body.innerHTML = `${this.nome}`
+    }
+}
